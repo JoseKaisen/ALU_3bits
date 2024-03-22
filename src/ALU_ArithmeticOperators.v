f6900c7a67ea4c9b8ee2c5c7eb2ba8f101e70da1
JoseKaisen <164433211+JoseKaisen@users.noreply.github.com>
2024-03-22 15:08:56 -0600

    Update ALU_ArithmeticOperators.v

--- src/ALU_ArithmeticOperators.v
+++ src/ALU_ArithmeticOperators.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module arithmetic_operators(input [2:0]A,B, output reg [3:0]Y1_ADD, output reg[5:0]Y3_MULT, 
+module arithmetic_operators (input [2:0]A,B, output reg [3:0]Y1_ADD, output reg[5:0]Y3_MULT, 
                                   output reg[2:0]Y2_MIN, Y4_DIV, Y5_MOD);
 always@(A,B)
 begin
